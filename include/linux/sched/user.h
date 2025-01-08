6df665ae07f6a6c2af8af60a47154c767b20abf4
simonpunk <simonpunk2016@gmail.com>
2025-01-08 19:19:45 +0700

    susfs: SUS_PATH: Refactor part of the code to fix random reboot for some xiaomi/redmi devices and small enhancement
    - Looks like using user_struct->android_kabi_reserved2 will cause the random bootloop for some of the xiaomi/redmi devices on kernel 13-5.15, and changing from user_struct->android_kabi_reserved2 to task_struct->android_kabi_reserved7 seems to fix the issue
    - A new file linux/include/susfs_def.h is added for the ENUM definition only, and now we can include this header without including other extra susfs functions which may somehow cause bootloop as well for unknown reason as tested in the past, but it should be fine now by doing this, and we should use ANDROID_KABI_USE / ANDROID_KABI_USE2 for better code reading in future
    - Added extra hook for filldir() in fs/readdir.c
    - Filesystem like tmpfs or fuse cannot be added to sus_path
    - Also, it is strongly suggested that the SUS_OVERLAYFS feature SHOULD be disabled if we are building for some xiaomi/redmi/oneplus devices that has default overlay mounts by the ROM itself
    - Update README.md for new instruction
    - Thanks for @Pzqqt (github) for the suggestion of better KABI naming
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- include/linux/sched/user.h
+++ include/linux/sched/user.h
@@ -50,11 +50,6 @@ struct user_struct {
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
-
-#ifdef CONFIG_KSU_SUSFS
-	u64 android_kabi_reserved2;
-#endif
-
 };
 
 extern int uids_sysfs_init(void);
